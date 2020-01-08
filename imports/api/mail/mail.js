0deb386d4d702e734bcf5d5788d7e4c30d869639
jk <jey.and.key@gmail.com>
2020-01-08 06:36:35 +0000

    commit

--- imports/api/mail/mail.js
+++ imports/api/mail/mail.js
@@ -4,14 +4,14 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Users } from '../cols.js';
 import mailgun from 'mailgun-js'
-const KEY = "key-addcac9154e09fa36a6aac8021bf08e6"
-const DOMAIN = "sandboxb74894d472784e0f8a12e3ae6f399185.mailgun.org"
-const testEmail = "jey.and.key@gmail.com"
-// const KEY = process.env.MAILGUN_API_KEY
-// const DOMAIN = process.env.MAILGUN_DOMAIN
-// const testEmail = "email@grape-develop.dk"
+// const KEY = "key-addcac9154e09fa36a6aac8021bf08e6"
+// const DOMAIN = "sandboxb74894d472784e0f8a12e3ae6f399185.mailgun.org"
+// const testEmail = "jey.and.key@gmail.com"
+const KEY =  process.env.MAILGUN_API_KEY
+const DOMAIN = "grape-develop.dk"
+const testEmail = "email@grape-develop.dk"
 
-const mg = mailgun({ apiKey: KEY, domain: DOMAIN });
+const mg = mailgun({ apiKey: KEY, domain: DOMAIN, host: 'api.eu.mailgun.net' });
 
 Meteor.methods({
   'mail.create' ({ recepients, body, subject }) {
@@ -23,6 +23,7 @@ Meteor.methods({
   },
   'mail.send' ( {body= 'asdf', subject = 'asdf', recepient = testEmail} ) {
     this.unblock()
+    console.log(KEY)
     const data = {
       from: 'Excited User <me@samples.mailgun.org>',
       to: recepient,
